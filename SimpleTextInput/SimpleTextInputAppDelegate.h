9ce68fccc6bbe5fee444d82299c7e11461533f56
bradley <bradley.j.griffith@gmail.com>
2013-04-14 19:03:11 -0500

    Initial Commit

---
+++ SimpleTextInput/SimpleTextInputAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SimpleTextInputAppDelegate.h
+//  SimpleTextInput
+//
+//  Created by Bradley Griffith on 4/14/13.
+//  Copyright (c) 2013 Bradley Griffith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTextInputAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleTextInput/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleTextInput
+//
+//  Created by Bradley Griffith on 4/14/13.
+//  Copyright (c) 2013 Bradley Griffith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleTextInputAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleTextInputAppDelegate class]));
+    }
+}
