56195d8b014bb4edf421498ebff7428befb46196
bradley <bradley.j.griffith@gmail.com>
2013-04-17 15:24:21 -0500

    adding control buttons

--- InputViewController.h
+++ InputViewController.h
@@ -15,10 +15,17 @@
 {
     
 }
+@property (weak, nonatomic) IBOutlet UIToolbar *keyboardActionsBar;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
+@property (weak, nonatomic) IBOutlet UILabel *cancelButton;
 
 @property (weak, nonatomic) IBOutlet StarfieldView *background;
 @property (weak, nonatomic) IBOutlet UITextView *descriptionText;
 @property (nonatomic) CADisplayLink *displayLink;
 @property (nonatomic) CGRect defaultTextFrame;
+@property (nonatomic) CGRect defaultActionBarFrame;
 @property (nonatomic) UIColor *defaultTextBackground;
+@property (nonatomic) NSString *backupText;
+
+- (IBAction)saveDescription:(id)sender;
 @end
--- InputViewController.m
+++ InputViewController.m
@@ -27,18 +27,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(gameLoop)];
     [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
     
     _defaultTextFrame = _descriptionText.frame;
     _defaultTextBackground = _descriptionText.backgroundColor;
+    
+    UIImage *resizableButton = [[UIImage imageNamed:@"greenButton.png" ] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 20, 10, 20)];
+    UIImage *resizableButtonHighlighted = [[UIImage imageNamed:@"greenButtonHighlighted.png" ] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 20, 10, 20)];
+    [_saveButton setBackgroundImage:resizableButton forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [_saveButton setBackgroundImage:resizableButtonHighlighted forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [_saveButton setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys: [UIFont fontWithName:@"Helvetica" size:17.0], UITextAttributeFont, [UIColor clearColor], UITextAttributeTextShadowColor, nil] forState:UIControlStateNormal];
+    
+    [_cancelButton setTextColor:[UIColor colorWithRed:234/255.0 green:51/255.0 blue:66/255.0 alpha:1]];
+    _cancelButton.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(cancelEdit)];
+    [_cancelButton addGestureRecognizer:tapGesture];
+
+    
+    
+    CGRect actionBarFrame = [_keyboardActionsBar frame];
+    actionBarFrame.origin.y += actionBarFrame.size.height;
+    _keyboardActionsBar.frame = actionBarFrame;
+    _defaultActionBarFrame = actionBarFrame;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShown:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeShown:) name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
-
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -63,27 +81,34 @@
     [UIView setAnimationCurve:animationCurve];
     
     if (up) {
-        CGRect newFrame = _descriptionText.frame;
-        CGRect keyboardFrame = [self.view convertRect:keyboardEndFrame toView:nil];
+        CGRect newTextFrame = _descriptionText.frame;
+        CGRect newActionBarFrame = _keyboardActionsBar.frame;
         CGRect screenFrame = [[UIScreen mainScreen] applicationFrame];
-        newFrame.size.width = screenFrame.size.width;
-        newFrame.size.height = screenFrame.size.height - keyboardFrame.size.height;
-        newFrame.origin.x = 0;
-        newFrame.origin.y = 0;
-        _descriptionText.frame = newFrame;
+        
+        CGRect keyboardFrame = [self.view convertRect:keyboardEndFrame toView:nil];
+        
+        newTextFrame.size.width = screenFrame.size.width;
+        newTextFrame.size.height = screenFrame.size.height - keyboardFrame.size.height - newActionBarFrame.size.height;
+        newTextFrame.origin.x = 0;
+        newTextFrame.origin.y = 0;
+        _descriptionText.frame = newTextFrame;
         _descriptionText.backgroundColor = [UIColor whiteColor];
 
+        newActionBarFrame.origin.y = newTextFrame.size.height;
+        _keyboardActionsBar.frame = newActionBarFrame;
     }
     else {
         _descriptionText.frame = _defaultTextFrame;
         _descriptionText.backgroundColor = _defaultTextBackground;
+        _keyboardActionsBar.frame = _defaultActionBarFrame;
     }
     
     [UIView commitAnimations];
 }
 
-- (void)keyboardWillShown:(NSNotification*)aNotification
+- (void)keyboardWillBeShown:(NSNotification*)aNotification
 {
+    _backupText = _descriptionText.text;
     [self moveTextViewForKeyboard:aNotification up:YES];
 }
 
@@ -92,11 +117,18 @@
     [self moveTextViewForKeyboard:aNotification up:NO];
 }
 
+- (IBAction)saveDescription:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (void)cancelEdit {
+    [self.view endEditing:YES];
+    _descriptionText.text = _backupText;
+}
+
 -(void)gameLoop
 {
     [_background moveShapes];
-    
-    //Here you could add your collision detection code
 }
 
 - (void)didReceiveMemoryWarning
